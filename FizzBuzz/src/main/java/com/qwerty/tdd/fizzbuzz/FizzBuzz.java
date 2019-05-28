1621f1190ffeae4f6b4a0b4fde4e332eee2f3270
Fiore Mario Vitale <mvitale86@gmail.com>
2019-05-28 16:15:54 +0200

    [FizzBuzz] Green: Added minimum code to let test for 10 (trying to test multiple of 10 case) to pass.

--- FizzBuzz/src/main/java/com/qwerty/tdd/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/main/java/com/qwerty/tdd/fizzbuzz/FizzBuzz.java
@@ -40,6 +40,7 @@ public class FizzBuzz {
 
         if( number % 3 == 0) return "Fizz";
         if( number == 5) return "Buzz";
+        if( number == 10) return "Buzz";
 
         return String.valueOf(number);
     }
