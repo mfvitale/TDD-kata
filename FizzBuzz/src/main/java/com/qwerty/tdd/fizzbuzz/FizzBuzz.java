9e4d38d65353dc55404aff19972f3c7f88b30361
Fiore Mario Vitale <mvitale86@gmail.com>
2019-05-28 16:13:35 +0200

    [FizzBuzz] Green: Added minimum code to let the test on the "Fizz" case pass.

--- FizzBuzz/src/main/java/com/qwerty/tdd/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/main/java/com/qwerty/tdd/fizzbuzz/FizzBuzz.java
@@ -39,6 +39,7 @@ public class FizzBuzz {
     public String convert(int number) {
 
         if( number % 3 == 0) return "Fizz";
+        if( number == 5) return "Buzz";
 
         return String.valueOf(number);
     }
