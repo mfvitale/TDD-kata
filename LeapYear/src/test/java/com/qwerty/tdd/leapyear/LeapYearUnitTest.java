122ec14825a69fc282f043d62a5b7b2ff66484d7
Fiore Mario Vitale <mvitale86@gmail.com>
2019-05-29 11:54:13 +0200

    [LeapYear] Red: Added a test that fails due to a compilation error.

---
+++ LeapYear/src/test/java/com/qwerty/tdd/leapyear/LeapYearUnitTest.java
@@ -0,0 +1,14 @@
+package com.qwerty.tdd.leapyear;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class LeapYearUnitTest {
+
+    @Test
+    public void check_1997_is_not_a_leap_year() {
+
+        assertFalse(new Year().isLeapYear());
+    }
+}
