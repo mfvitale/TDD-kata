a76ef28d0ee028c3dfbf98faa4318a7d4ab7729c
Fiore Mario Vitale <mvitale86@gmail.com>
2019-05-30 10:05:26 +0200

    [LeapYear] Red: Added a new failing test for testing a leap year

--- LeapYear/src/test/java/com/qwerty/tdd/leapyear/LeapYearUnitTest.java
+++ LeapYear/src/test/java/com/qwerty/tdd/leapyear/LeapYearUnitTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearUnitTest {
 
@@ -11,5 +12,11 @@ public class LeapYearUnitTest {
     public void check_1997_is_not_a_leap_year() {
 
         assertFalse(new Year().isLeapYear());
+    }
+
+    @Test
+    public void check_1996_is_a_leap_year() {
+
+        assertTrue(new Year().isLeapYear());
     }
 }
