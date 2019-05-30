f3ddc90e79ecf3a7facd87cc5c47b7cf2b1f7a54
Fiore Mario Vitale <mvitale86@gmail.com>
2019-05-30 10:03:22 +0200

    [LeapYear] Green: Added simple implementation (Faking it). Test now pass!

---
+++ LeapYear/src/main/java/com/qwerty/tdd/leapyear/Year.java
@@ -0,0 +1,9 @@
+package com.qwerty.tdd.leapyear;
+
+public class Year {
+
+    public boolean isLeapYear() {
+
+        return false;
+    }
+}
--- LeapYear/src/test/java/com/qwerty/tdd/leapyear/LeapYearUnitTest.java
+++ LeapYear/src/test/java/com/qwerty/tdd/leapyear/LeapYearUnitTest.java
@@ -2,6 +2,7 @@ package com.qwerty.tdd.leapyear;
 
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class LeapYearUnitTest {
